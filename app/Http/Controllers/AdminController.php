340494c5d7fb11954dac2a827db36c6de758e150
faisalalambd <faisal.alamm.bd@gmail.com>
2025-06-15 23:58:10 +0600

    Admin Controller -> AdminLoginSubmit Method

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,18 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class AdminController extends Controller
 {
-    public function AdminLogin(){
+    public function AdminLogin()
+    {
         return view('admin.login');
     } //End Method
 
-    public function AdminDashboard(){
+    public function AdminDashboard()
+    {
         return view('admin.dashboard');
     } //End Method
 
-    public function AdminLoginSubmit(Request $request){
-        
+    public function AdminLoginSubmit(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $check = $request->all();
+
+        $data = [
+            'email' => $check['email'],
+            'password' => $check['password'],
+        ];
+
+        if (Auth::guard('admin')->attempt($data)) {
+            return redirect()->route('admin.dashboard')->with('success', 'Login Successfully');
+        } else {
+            return redirect()->route('admin.login')->with('error', 'Invalid Credentials');
+        }
     } //End Method
 }
