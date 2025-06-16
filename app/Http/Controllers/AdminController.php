5f207140f61ecbeea94eeeb6d0a04c01a1696ffb
faisalalambd <faisal.alamm.bd@gmail.com>
2025-06-16 19:51:29 +0600

    Admin Forget Password -> WebsiteMail + email page + AdminForgetPasswordSubmit method

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use App\Mail\WebsiteMail;
+use Illuminate\Support\Facades\Mail;
 
 class AdminController extends Controller
 {
@@ -50,7 +53,29 @@ class AdminController extends Controller
         return view('admin.forget_password');
     } //End Method
 
-    public function AdminForgetPasswordSubmit(Request $request) {
-        
+    public function AdminForgetPasswordSubmit(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        $admin_data = Admin::where('email', $request->email)->first();
+
+        if (!$admin_data) {
+            return redirect()->back()->with('error', 'Email Not Found');
+        }
+
+        $token = hash('sha256', time());
+        $admin_data->token = $token;
+        $admin_data->update();
+
+        $reset_link = url('admin/reset-password/' . $token . '/' . $request->email);
+        $subject = 'Reset Password';
+        $message = 'Please Click on below link to reset password <br>';
+        $message .= "<a href='" . $reset_link . "'> Click Here </a>";
+
+        Mail::to($request->email)->send(new WebsiteMail($subject, $message));
+
+        return redirect()->back()->with('success', 'Reset Password Link Send On Your Email');
     } //End Method
 }
