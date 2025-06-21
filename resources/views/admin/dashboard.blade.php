7f0b09a2e86a498c0c86e1b509edc00f3cdc7f87
faisalalambd <faisal.alamm.bd@gmail.com>
2025-06-21 12:43:59 +0600

    Admin Login -> Customize Page

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -16,7 +16,7 @@
     {{-- Plugin CSS --}}
     <link href="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet" type="text/css" />
     {{-- Preloader CSS --}}
-    <link rel="stylesheet" href="{{ asset('backend/assets/css/preloader.min.css') }}" type="text/css" />
+    {{-- <link rel="stylesheet" href="{{ asset('backend/assets/css/preloader.min.css') }}" type="text/css" /> --}}
     {{-- Bootstrap CSS --}}
     <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
     {{-- Icons CSS --}}
@@ -62,7 +62,7 @@
     <script src="{{ asset('backend/assets/libs/metismenu/metisMenu.min.js') }}"></script>
     {{-- Simple Bar JS --}}
     <script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
-    {{-- Waves JS --}}
+    {{-- Node Waves JS --}}
     <script src="{{ asset('backend/assets/libs/node-waves/waves.min.js') }}"></script>
     {{-- Feather Icons JS --}}
     <script src="{{ asset('backend/assets/libs/feather-icons/feather.min.js') }}"></script>
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,39 +1,147 @@
-<html>
-<title>Admin Login Page</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+<!doctype html>
+<html lang="en">
 
-<body class="container">
-    <h1>Admin Login</h1>
+<head>
 
-    @if ($errors->any())
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    @endif
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
 
-    @if (Session::has('error'))
-        <li>{{ Session::get('error') }}</li>
-    @endif
+    <title>Admin Login</title>
 
-    @if (Session::has('success'))
-        <li>{{ Session::get('success') }}</li>
-    @endif
+    {{-- Favicon Icon --}}
+    <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.ico') }}">
+    {{-- Preloader CSS --}}
+    {{-- <link rel="stylesheet" href="{{ asset('backend/assets/css/preloader.min.css') }}" type="text/css" /> --}}
+    {{-- Bootstrap CSS --}}
+    <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    {{-- Icons CSS --}}
+    <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    {{-- App CSS --}}
+    <link href="{{ asset('backend/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 
-    <form action="{{ route('admin.login.submit') }}" method="post">
-        @csrf
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+</head>
+
+<body>
+
+    <div class="auth-page">
+
+        <div class="container-fluid p-0">
+
+            <div class="d-flex align-items-center justify-content-center min-vh-100" style="position: relative;">
+                <div class="bg-overlay bg-primary" style="opacity: 0.9; position: absolute; top:0; left:0; width:100%; height:100%; z-index:1;"></div>
+                <ul class="bg-bubbles" style="z-index:2; position:absolute; width:100%; height:100%; left:0; top:0; pointer-events:none;">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <div class="auth-full-page-content d-flex p-4" style="z-index:3; min-width:350px; max-width:450px; width:100%;">
+                    <div class="w-100">
+                        <div class="d-flex flex-column h-100">
+                            <div class="auth-content my-auto">
+                                <div class="card shadow">
+                                    <div class="card-body">
+
+                                        <div class="m-3 m-md-4 text-center">
+                                            <a href="index.html" class="d-block auth-logo">
+                                                <img src="{{ asset('backend/assets/images/logo-sm.svg') }}" alt="" height="28"> <span class="logo-txt">Minia</span>
+                                            </a>
+                                        </div>
+
+                                        <div class="text-center">
+                                            <h5 class="mb-0">Welcome Back !</h5>
+                                            <p class="text-muted mt-2">Sign in to continue to Admin Dashboard.</p>
+                                        </div>
+
+                                        @if ($errors->any())
+                                            <ul class="text-danger mt-4">
+                                                @foreach ($errors->all() as $error)
+                                                    <li>{{ $error }}</li>
+                                                @endforeach
+                                            </ul>
+                                        @endif
+
+                                        @if (Session::has('error'))
+                                            <ul class="text-danger mt-4">
+                                                <li>{{ Session::get('error') }}</li>
+                                            </ul>
+                                        @endif
+                                        @if (Session::has('success'))
+                                            <ul class="text-success mt-4">
+                                                <li>{{ Session::get('success') }}</li>
+                                            </ul>
+                                        @endif
+                                        <form class="mt-4 pt-2" action="{{ route('admin.login.submit') }}" method="post">
+                                            @csrf
+
+                                            <div class="mb-3">
+                                                <label class="form-label">Email</label>
+                                                <input type="email" name="email" class="form-control" id="email" placeholder="Enter Email">
+                                            </div>
+                                            <div class="mb-3">
+                                                <div class="d-flex align-items-start">
+                                                    <div class="flex-grow-1">
+                                                        <label class="form-label">Password</label>
+                                                    </div>
+                                                    <div class="flex-shrink-0">
+                                                        <div class="">
+                                                            <a href="{{ route('admin.forget.password') }}" class="text-muted">Forgot password?</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="input-group auth-pass-inputgroup">
+                                                    <input type="password" name="password" class="form-control" placeholder="Enter password" aria-label="Password" aria-describedby="password-addon">
+                                                    <button class="btn btn-light shadow-none ms-0" type="button" id="password-addon"><i class="mdi mdi-eye-outline"></i></button>
+                                                </div>
+                                            </div>
+                                            <div class="mb-3">
+                                                <button class="btn btn-primary w-100 waves-effect waves-light" type="submit">Log In</button>
+                                            </div>
+                                        </form>
+
+                                        <div class="mt-4 text-center">
+                                            <p class="text-muted mb-0">Don't have an account ? <a href="auth-register.html" class="text-primary fw-semibold"> Signup now </a> </p>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" name="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-            <a href="{{ route('admin.forget.password') }}">Forget Password</a>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+
+    </div>
+
+
+    {{-- jQuery --}}
+    <script src="{{ asset('backend/assets/libs/jquery/jquery.min.js') }}"></script>
+    {{-- Bootstrap JS --}}
+    <script src="{{ asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    {{-- Metis Menu JS --}}
+    <script src="{{ asset('backend/assets/libs/metismenu/metisMenu.min.js') }}"></script>
+    {{-- Simple Bar JS --}}
+    <script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
+    {{-- Node Waves JS --}}
+    <script src="{{ asset('backend/assets/libs/node-waves/waves.min.js') }}"></script>
+    {{-- Feather Icons JS --}}
+    <script src="{{ asset('backend/assets/libs/feather-icons/feather.min.js') }}"></script>
+    {{-- Pace JS --}}
+    <script src="{{ asset('backend/assets/libs/pace-js/pace.min.js') }}"></script>
+    {{-- Password Adoon Init JS --}}
+    <script src="{{ asset('backend/assets/js/pages/pass-addon.init.js') }}"></script>
+
 </body>
 
 </html>
